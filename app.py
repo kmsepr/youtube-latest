ef0f07ed7054d6bcaf1638ef9e9e9a0bc7d64cd7
kmsepr <95290980+kmsepr@users.noreply.github.com>
2025-03-18 10:32:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 COOKIES_PATH = "/mnt/data/cookies.txt"
 
 # List of YouTube playlists
-YOUTUBE_PLAYLISTS = [
-    "https://youtube.com/playlist?list=PLGwUmcHSMM44vCWbO_LSLfFHKwZABQ3Ck",
-    # Add more playlists here if needed
-]
+PLAYLISTS = {
+    "playlist1": "https://www.youtube.com/playlist?list=PLWzDl-O4zlwSDM6PAMsGgFNCPsvQk-2aN",
+    "playlist2": "https://youtube.com/playlist?list=PLGwUmcHSMM44vCWbO_LSLfFHKwZABQ3Ck",
+}
 
 def generate_audio():
     for playlist_url in YOUTUBE_PLAYLISTS:
